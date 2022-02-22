ecfa1a4237ea604216a1292ef608ba4d854c8589
HuskyHsu <schi.hsu@gmail.com>
2022-02-22 22:27:31 +0800

    feat: 🎸 update regional data format

--- scripts/wiki52_format.js
+++ scripts/wiki52_format.js
@@ -15,10 +15,13 @@ JSON.stringify(
       }
 
       let isHisui = false;
-
+      let isAlola = false;
       if (tr[3].endsWith("洗翠的樣子")) {
-        tr[3] = tr[3].replace("\\n洗翠的樣子", "");
+        tr[3] = tr[3].replace("\n洗翠的樣子", "");
         isHisui = true;
+      } else if (tr[3].endsWith("阿羅拉的樣子")) {
+        tr[3] = tr[3].replace("\n阿羅拉的樣子", "");
+        isAlola = true;
       }
 
       tr.splice(2, 1);
@@ -27,12 +30,18 @@ JSON.stringify(
         types.push(tr[4]);
       }
 
-      return {
+      const data = {
         id: tr[0],
         pid: tr[1],
         name: tr[2],
         types: types,
-        isHisui,
       };
+
+      if (isHisui) {
+        data.regional = "洗翠";
+      } else if (isAlola) {
+        data.regional = "阿羅拉";
+      }
+      return data;
     })
 );
