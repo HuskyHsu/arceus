b4e4eba9640802ddeb1eb486ff55d5b74934455f
HuskyHsu <schi.hsu@gmail.com>
2022-03-21 13:34:49 +0800

    feat: 🎸 update evolution data

--- scripts/clean.py
+++ scripts/clean.py
@@ -3,10 +3,12 @@ from typing import List, Optional, Union
 
 from pydantic import BaseModel
 
+
 class Obtain(BaseModel):
     location: Union[None, str, List[str]]
     mode: str
     remark: Optional[str] = None
+
 
 class Stats(BaseModel):
     HP: int
@@ -17,9 +19,11 @@ class Stats(BaseModel):
     Speed: int
     TOTAL: int
 
+
 class Evolution(BaseModel):
     name: str
     source: Optional[str] = None
+
 
 class Pokemon(BaseModel):
     id: int
@@ -29,8 +33,20 @@ class Pokemon(BaseModel):
     alt_form: Optional[List[str]] = None
     obtain: List[Obtain]
     # props: List[str] = []
-    stats: Union[Stats, List[Stats]] = None
+    stats: Union[list[int], list[list[int]]]  # Stats, List[Stats],
     # evolution: Optional[Evolution] = None
+
+
+def location_code(name):
+    areas = ["祝慶村", "黑曜原野", "紅蓮濕地", "群青海岸", "天冠山麓", "純白凍土"]
+    if name in areas:
+        return f'{areas.index(name)}'
+
+    for i, area_name in enumerate(areas):
+        if name in area[area_name]:
+            return f'{i}-{area[area_name].index(name)}'
+
+    return name
 
 
 if __name__ == '__main__':
@@ -41,6 +57,9 @@ if __name__ == '__main__':
     with open('../src/data/fullInfo.json', 'rt', encoding='utf-8') as fin:
         all_pm_2 = json.load(fin)
 
+    with open('../src/data/evolution.json', 'rt', encoding='utf-8') as fin:
+        evolution = json.load(fin)
+
     with open('../src/data/area.json', 'rt', encoding='utf-8') as fin:
         area = json.load(fin)
 
@@ -48,11 +67,18 @@ if __name__ == '__main__':
     for pm in all_pm[:]:
         pm['obtain'] = [
             Obtain(
-                location = obtain[0].split('，') if '，' in obtain[0] else (None if obtain[0] == '' else obtain[0]),
-                mode = obtain[1],
-                remark = obtain[2] if len(obtain) > 2 else None
+                location=[location_code(l) for l in obtain[0].split('，')] if '，' in obtain[0] else (
+                    None if obtain[0] == '' else location_code(obtain[0])),
+                mode=obtain[1],
+                remark=obtain[2] if len(obtain) > 2 else None
             ) for obtain in pm['obtain']
         ]
+
+        evos = [e for e in evolution if e['after'] == pm['name']]
+        if len(evos) > 0:
+            pm['obtain'] += [
+                Obtain(location=None, mode='進化', remark=evo['require']) for evo in evos
+            ]
 
         pm['props'] = [
             prop[0] if len(prop) == 1 else '，'.join(prop) for prop in pm['props']
@@ -61,28 +87,37 @@ if __name__ == '__main__':
         pms = [pm_2 for pm_2 in all_pm_2 if pm_2['id'] == pm['id']]
         if len(pms) == 1:
             pm_stats = pms[0]['stats']
-            pm['stats'] = Stats(
-                HP = pm_stats['HP'],
-                Attack = pm_stats['Attack'],
-                Defense = pm_stats['Defense'],
-                Sp_Atk = pm_stats['Sp. Atk'],
-                Sp_Def = pm_stats['Sp. Def'],
-                Speed = pm_stats['Speed'],
-                TOTAL = pm_stats['TOTAL']
-            )
+            # pm['stats'] = Stats(
+            #     HP=pm_stats['HP'],
+            #     Attack=pm_stats['Attack'],
+            #     Defense=pm_stats['Defense'],
+            #     Sp_Atk=pm_stats['Sp. Atk'],
+            #     Sp_Def=pm_stats['Sp. Def'],
+            #     Speed=pm_stats['Speed'],
+            #     TOTAL=pm_stats['TOTAL']
+            # )
+            pm['stats'] = [pm_stats['HP'], pm_stats['Attack'],
+                           pm_stats['Defense'], pm_stats['Sp. Atk'],
+                           pm_stats['Sp. Def'], pm_stats['Speed']]
             if 'regional' in pm:
                 pm['alt_form'] = [pm['regional'] + '的樣子']
 
         elif 'alt_form' in pm and len(pm['alt_form']) == len(pms):
-            pm['stats'] = [Stats(
-                HP = pm_['stats']['HP'],
-                Attack = pm_['stats']['Attack'],
-                Defense = pm_['stats']['Defense'],
-                Sp_Atk = pm_['stats']['Sp. Atk'],
-                Sp_Def = pm_['stats']['Sp. Def'],
-                Speed = pm_['stats']['Speed'],
-                TOTAL = pm_['stats']['TOTAL']
-            ) for pm_ in pms]
+            # pm['stats'] = [Stats(
+            #     HP=pm_['stats']['HP'],
+            #     Attack=pm_['stats']['Attack'],
+            #     Defense=pm_['stats']['Defense'],
+            #     Sp_Atk=pm_['stats']['Sp. Atk'],
+            #     Sp_Def=pm_['stats']['Sp. Def'],
+            #     Speed=pm_['stats']['Speed'],
+            #     TOTAL=pm_['stats']['TOTAL']
+            # ) for pm_ in pms]
+
+            pm['stats'] = [
+                [pm_['stats']['HP'], pm_['stats']['Attack'],
+                 pm_['stats']['Defense'], pm_['stats']['Sp. Atk'],
+                 pm_['stats']['Sp. Def'], pm_['stats']['Speed']
+                 ] for pm_ in pms]
         else:
             print(pm['name'])
             print(json.dumps(pms, ensure_ascii=False))
@@ -97,4 +132,5 @@ if __name__ == '__main__':
         #     pm_ = pm_[0]
 
     with open('../src/data/pokemon.json', 'wt', encoding='utf-8') as fout:
-        fout.write(json.dumps([pm.dict(exclude_none = True) for pm in all_data], ensure_ascii=False))
+        fout.write(json.dumps([pm.dict(exclude_none=True)
+                   for pm in all_data], ensure_ascii=False))
