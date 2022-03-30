f89c0a7a12bba2e21734658eb23991a52e90570e
HuskyHsu <schi.hsu@gmail.com>
2022-03-30 10:15:44 +0800

    fix: 🐛 react call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
 import { defaultTypeTrue } from "./utils/status";
-
-import { BaseInfo } from "./components/BaseInfo";
-import { SearchBar } from "./components/SearchBar";
 import { api } from "./data";
 import { Pokemon } from "./models";
-
-const allPM = await api<Pokemon[]>("/arceus/data/pokemon.json");
+import { BaseInfo } from "./components/BaseInfo";
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
   const [filter, setFilter] = useState({
     types: defaultTypeTrue,
     keyword: "",
     area: "全區域",
     areaSelector: false,
   });
+
+  useEffect(() => {
+    api<Pokemon[]>("/arceus/data/pokemon.json").then((data) => {
+      setPokemons(data);
+    });
+  }, []);
 
   return (
     <article className="flex flex-col justify-center items-center my-16 gap-8">
@@ -27,9 +32,8 @@ function App() {
         className={clsx(
           "flex justify-center items-center flex-wrap content-center",
           "gap-x-2 gap-y-4 w-full md:w-5/6 max-w-5xl"
-        )}
-      >
-        {allPM.map((pm) => (
+        )}>
+        {pokemons.map((pm) => (
           <BaseInfo
             key={`${pm.pid}${pm.alt_form ?? ""}`}
             pm={pm}
