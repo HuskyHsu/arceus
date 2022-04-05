9d735c3faa06e8ba17271b0c4320f79c2e91a5bc
HuskyHsu <schi.hsu@gmail.com>
2022-04-05 22:33:14 +0800

    feat: 🎸 upate radar chart

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { api } from "@/utils/http";
 import { BASE_URL } from "@/utils/const";
 import { Radar } from "./components/radarChart";
+import clsx from "clsx";
 
 interface Info {
   types: string[];
@@ -32,7 +33,7 @@ function Detail() {
     info: [
       {
         types: [],
-        stats: [255, 255, 255, 255, 255, 255],
+        stats: [0, 0, 0, 0, 0, 0],
       },
     ],
   });
@@ -45,20 +46,31 @@ function Detail() {
   }, []);
 
   return (
-    <>
-      <article className="grid grid-cols-2 gap-4">
-        <section>
-          種族值
-          <Radar stats={pokemon.info[0].stats} />
+    <article className="flex flex-col justify-center items-center my-16">
+      <article className="w-5/6 max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-4">
+        <section className="">
+          <div>
+            <Radar stats={pokemon.info[0].stats} />
+          </div>
         </section>
-        <section>(顯圖+名稱+屬性)</section>
-        <section>出沒地點</section>
-        <section>攜帶道具</section>
-        <section>升等招式</section>
-        <section>傳授招式</section>
-        <section className="col-span-2">進化表</section>
+        <section className="">
+          <div>
+            (顯圖+名稱+屬性)
+            <img
+              src={`${BASE_URL}image/pokemon/0843_000_041_n.png`}
+              loading="lazy"
+              alt=""
+              className={clsx("max-w-none w-96 rounded-full")}
+            />
+          </div>
+        </section>
+        <section className="">出沒地點</section>
+        <section className="">攜帶道具</section>
+        <section className="">升等招式</section>
+        <section className="">傳授招式</section>
+        <section className="md:col-span-2">進化表</section>
       </article>
-    </>
+    </article>
   );
 }
 
--- src/pages/Detail/components/radarChart.tsx
+++ src/pages/Detail/components/radarChart.tsx
@@ -1,3 +1,18 @@
+interface RadarPorps {
+  stats: number[];
+}
+
+interface LinePorps {
+  deg: number;
+  radius: number;
+  center: number[];
+}
+
+interface TextPorps extends LinePorps {
+  text: string;
+  value: number;
+}
+
 const getPosition = (deg: number, radius: number, center: number[]) => {
   const radians = (deg * 2 * Math.PI) / 360;
   return [
@@ -6,60 +21,112 @@ const getPosition = (deg: number, radius: number, center: number[]) => {
   ];
 };
 
-interface Porps {
-  stats: number[];
-}
+const Line = ({ deg, radius, center }: LinePorps) => {
+  const point1 = getPosition(deg, radius, center);
+  const point2 = getPosition(deg + 180, radius, center);
 
-export function Radar({ stats: [hp, att, def, spAtk, spDef, speed] }: Porps) {
+  return (
+    <line
+      x1={point1[0]}
+      y1={point1[1]}
+      x2={point2[0]}
+      y2={point2[1]}
+      stroke="#FFFFFF"
+      strokeWidth="2"
+    />
+  );
+};
+
+const Text = ({ deg, radius, center, text, value }: TextPorps) => {
+  const point = getPosition(deg, radius * 1.1, center);
+
+  return (
+    <>
+      <text x={point[0]} y={point[1] - 3} fontSize="0.5em" textAnchor="middle">
+        {text}
+      </text>
+      <text x={point[0]} y={point[1] + 7} fontSize="0.5em" textAnchor="middle">
+        {value}
+      </text>
+    </>
+  );
+};
+
+export function Radar({
+  stats: [hp, att, def, spAtk, spDef, speed],
+}: RadarPorps) {
   const radius = 100;
   const maxValue = 255;
+  const scale = radius / maxValue;
   const center = [radius * 1.4, radius * 1.4];
 
-  const pgPoints = [...Array(6).keys()]
+  const bgPoints = [...Array(6).keys()]
     .map((_, i) => 30 + i * 60)
     .map((deg) => getPosition(deg, radius, center));
 
   const speciesStrength = [
-    getPosition(30, (def * radius) / maxValue, center),
-    getPosition(90, (speed * radius) / maxValue, center),
-    getPosition(150, (spDef * radius) / maxValue, center),
-    getPosition(210, (spAtk * radius) / maxValue, center),
-    getPosition(270, (hp * radius) / maxValue, center),
-    getPosition(330, (att * radius) / maxValue, center),
+    getPosition(270, (hp * radius) / 255, center),
+    getPosition(330, (att * radius) / 165, center),
+    getPosition(30, (def * radius) / 200, center),
+    getPosition(90, (speed * radius) / 150, center),
+    getPosition(150, (spDef * radius) / 200, center),
+    getPosition(210, (spAtk * radius) / 165, center),
   ];
 
   return (
-    <svg className="w-96" viewBox={`0 0 ${radius * 2.8} ${radius * 2.8}`}>
-      <polygon points={pgPoints.flat().join(", ")} fill="#e9e9e9" />
-      <line
-        x1={radius * 1.4}
-        y1={radius * 0.4}
-        x2={radius * 1.4}
-        y2={radius * 2.4}
-        stroke="#FFFFFF"
-        strokeWidth="2"
-      />
-      <line
-        x1={getPosition(30, radius, center)[0]}
-        y1={getPosition(30, radius, center)[1]}
-        x2={getPosition(210, radius, center)[0]}
-        y2={getPosition(210, radius, center)[1]}
-        stroke="#FFFFFF"
-        strokeWidth="2"
-      />
-      <line
-        x1={getPosition(-30, radius, center)[0]}
-        y1={getPosition(-30, radius, center)[1]}
-        x2={getPosition(150, radius, center)[0]}
-        y2={getPosition(150, radius, center)[1]}
-        stroke="#FFFFFF"
-        strokeWidth="2"
-      />
+    <svg
+      className="w-full h-auto"
+      viewBox={`0 0 ${center[0] * 2} ${center[1] * 2}`}
+    >
+      <polygon points={bgPoints.flat().join(", ")} fill="#e9e9e9" />
+      <g>
+        <Line deg={90} radius={radius} center={center} />
+        <Line deg={30} radius={radius} center={center} />
+        <Line deg={-30} radius={radius} center={center} />
+      </g>
       <polygon
         points={speciesStrength.flat().join(", ")}
         fill="#339DDF"
         fillOpacity="0.6"
       />
+      <g>
+        <Text text="HP" value={hp} deg={270} radius={radius} center={center} />
+        <Text
+          text="攻擊"
+          value={att}
+          deg={330}
+          radius={radius}
+          center={center}
+        />
+        <Text
+          text="防禦"
+          value={def}
+          deg={30}
+          radius={radius}
+          center={center}
+        />
+        <Text
+          text="速度"
+          value={speed}
+          deg={90}
+          radius={radius}
+          center={center}
+        />
+        <Text
+          text="特防"
+          value={spDef}
+          deg={150}
+          radius={radius}
+          center={center}
+        />
+        <Text
+          text="特攻"
+          value={spAtk}
+          deg={210}
+          radius={radius}
+          center={center}
+        />
+      </g>
     </svg>
   );
 }
