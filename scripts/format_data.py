d34e288b0e8572a21bdf217642c3ce39cc06761a
HuskyHsu <schi.hsu@gmail.com>
2022-04-09 21:51:47 +0800

    feat: 🎸 update full data

--- scripts/format_data.py
+++ scripts/format_data.py
@@ -1,7 +1,23 @@
 import json
+from os import path
 
-from requests import delete
-
+NameSuffix = {
+    "洗翠": "H",
+    "砂土蓑衣": "G",
+    "垃圾蓑衣": "S",
+    "晴天形態": "S",
+    "白條紋": "W",
+    "雌性": "F",
+    "阿羅拉": "A",
+    "加熱洛托姆": "O",
+    "清洗洛托姆": "W",
+    "結冰洛托姆": "R",
+    "旋轉洛托姆": "F",
+    "切割洛托姆": "L",
+    "靈獸形態": "T",
+    "起源形態": "O",
+    "天空形態": "S",
+}
 
 if __name__ == "__main__":
 
@@ -10,6 +26,27 @@ if __name__ == "__main__":
 
     with open("../src/data/move.json", "rt", encoding="utf-8") as fin:
         all_move = json.load(fin)
+
+    with open("../src/data/area.json", "rt", encoding="utf-8") as fin:
+        all_area = json.load(fin)
+
+    with open("../src/data/evolution.json", "rt", encoding="utf-8") as fin:
+        all_evolution = json.load(fin)
+
+    evolution_map = {}
+    for evolution in all_evolution[:]:
+        if evolution["before"] not in evolution_map:
+            evolution_map[evolution["before"]] = []
+
+        evolution_map[evolution["before"]].append(evolution)
+
+    for evolution in evolution_map.copy():
+        for sub_evolution in evolution_map[evolution]:
+            if sub_evolution["after"] in evolution_map:
+                sub_evolution["evolution"] = evolution_map[sub_evolution["after"]][:]
+
+    for evolution in all_evolution[:]:
+        evolution_map[evolution["after"]] = evolution_map[evolution["before"]]
 
     for pm in all_pm[:]:
         pm["learnset"] = {"levelingUp": [], "tutoring": []}
@@ -26,6 +63,23 @@ if __name__ == "__main__":
             move = [move for move in all_move if tutoring == move["name"]][0].copy()
             pm["learnset"]["tutoring"].append(move)
 
+        for getMethod in pm["getMethods"]:
+            if "location" not in getMethod:
+                continue
+
+            if type(getMethod["location"]) == str:
+                getMethod["location"] = all_area["area"][getMethod["location"]]
+                continue
+
+            keys = [key for key in getMethod["location"].keys()]
+            for key in keys:
+                area_name = all_area["area"][key]
+                sub_area = all_area[area_name]
+                getMethod["location"][area_name] = [
+                    sub_area[i] for i in getMethod["location"][key]
+                ]
+                del getMethod["location"][key]
+
         del pm["levelingUp"]
         del pm["tutoring"]
 
@@ -40,5 +94,50 @@ if __name__ == "__main__":
             )
         pm["items"] = items
 
-    with open("../public/data/pokemon_v3.json", "wt", encoding="utf-8") as fout:
+        if pm["name"] in evolution_map:
+            pm["evolution"] = evolution_map[pm["name"]]
+        else:
+            print(pm["name"])
+
+        pm["link"] = str(pm["pid"]).zfill(3) + (
+            (NameSuffix[pm["altForm"]] if pm["altForm"] in NameSuffix else "")
+            if "altForm" in pm
+            else ""
+        )
+
+        pm["genderDiff"] = path.exists(f"../public/image/pokemon/{pm['link']}_f.png")
+
+    base_map = {}
+    for pm in all_pm:
+        info = {
+            "id": pm["id"],
+            "pid": pm["pid"],
+            "name": pm["name"],
+            "types": pm["types"],
+            "genderDiff": pm["genderDiff"],
+        }
+        if "altForm" in pm:
+            info["altForm"] = pm["altForm"]
+
+        base_map[pm["name"]] = info
+
+    for pm in all_pm[:]:
+        print(pm["name"])
+        if "evolution" in pm:
+            for ev in pm["evolution"]:
+                if type(ev["before"]) == str:
+                    ev["before"] = base_map[ev["before"]]
+
+                if type(ev["after"]) == str:
+                    ev["after"] = base_map[ev["after"]]
+
+                if "evolution" in ev:
+                    for ev2 in ev["evolution"]:
+                        if type(ev2["before"]) == str:
+                            ev2["before"] = base_map[ev2["before"]]
+
+                        if type(ev2["after"]) == str:
+                            ev2["after"] = base_map[ev2["after"]]
+
+    with open("../public/data/pokemon_full.json", "wt", encoding="utf-8") as fout:
         fout.write(json.dumps(all_pm, ensure_ascii=False))
