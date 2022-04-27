61115c63d6b34e5b912dd7cb6d87a7ee4f0772f9
HuskyHsu <schi.hsu@gmail.com>
2022-04-27 17:34:29 +0800

    feat: 🎸 update evolution path

--- src/pages/Detail/components/Evolution.tsx
+++ src/pages/Detail/components/Evolution.tsx
@@ -18,32 +18,58 @@ export function Evolution() {
     : "grid-cols-3";
 
   let rows =
-    pokemon.evolution && pokemon.evolution.length > 1
-      ? "row-span-2"
+    pokemon.evolution && pokemon.evolution?.length > 1
+      ? pokemon.evolution?.length === 8
+        ? "row-[span_8_/_span_8]"
+        : "row-span-2"
       : "row-span-1";
 
   let keyId = 0;
 
   const evolutionPath = pokemon.evolution?.reduce((acc, evolution, i) => {
-    let rows_ = "";
-    if (rows === "row-span-1") {
-      rows_ =
-        evolution.evolution && evolution.evolution.length > 1
-          ? "row-span-2"
-          : "row-span-1";
+    let rowElement = [] as JSX.Element[];
+
+    let secRows = "row-span-1";
+    if (evolution.evolution?.length || 0 > 1) {
+      secRows = "row-span-2";
     }
 
-    acc = acc.concat([
-      <div key={keyId} className={rows_}>
-        <Avatars pm={evolution.before} />
-      </div>,
-      <p key={keyId + 1} className={rows_}>
+    if (i === 0) {
+      rowElement.push(
+        <div
+          key={keyId}
+          className={
+            rows === "row-span-1" && secRows === "row-span-2"
+              ? "row-span-2"
+              : rows
+          }>
+          <Avatars pm={evolution.before} />
+        </div>
+      );
+    }
+
+    rowElement = rowElement.concat([
+      <p
+        key={keyId + 1}
+        className={
+          rows === "row-span-1" && secRows === "row-span-2"
+            ? "row-span-2"
+            : "row-span-1"
+        }>
         {evolution.require}
       </p>,
-      <div key={keyId + 2} className={rows_}>
+      <div
+        key={keyId + 2}
+        className={
+          rows === "row-span-1" && secRows === "row-span-2"
+            ? "row-span-2"
+            : "row-span-1"
+        }>
         <Avatars pm={evolution.after} />
       </div>,
     ]);
+
+    acc = acc.concat(rowElement.slice(0));
     keyId += 3;
     if (evolution.evolution) {
       evolution.evolution.forEach((evolution_, j) => {
@@ -57,5 +83,9 @@ export function Evolution() {
     return acc;
   }, [] as JSX.Element[]);
 
-  return <div className={clsx("grid", cols)}>{evolutionPath}</div>;
+  return (
+    <div className={clsx("grid justify-center items-center", cols)}>
+      {evolutionPath}
+    </div>
+  );
 }
