0af787b7f0f20ff7718f85d5348e9fdd89e3371e
HuskyHsu <schi.hsu@gmail.com>
2022-05-09 23:38:06 +0800

    feat: 🎸 update ConvexHull

--- scripts/map/format.py
+++ scripts/map/format.py
@@ -2,6 +2,8 @@ import json
 
 import requests
 from bs4 import BeautifulSoup as bs
+import numpy as np
+from scipy.spatial import ConvexHull
 
 if __name__ == "__main__":
 
@@ -22,57 +24,75 @@ if __name__ == "__main__":
 
         spawntable[spawn["icon"]][spawn["tableID"]].append(spawn["coords"])
 
+    for key in spawntable["pokeball"].keys():
+        if len(spawntable["pokeball"][key]) <= 3:
+            continue
+
+        spawntable["pokeball"][key] = [
+            (row[0], row[1]) for row in spawntable["pokeball"][key]
+        ]
+        hull = ConvexHull(spawntable["pokeball"][key])
+        spawntable["pokeball"][key] = [
+            spawntable["pokeball"][key][i] for i in hull.vertices
+        ]
+
+    # generators = np.array([[0.2, 0.2],
+    #                    [0.2, 0.4],
+    #                    [0.4, 0.4],
+    #                    [0.4, 0.2],
+    #                    [0.3, 0.6]])
+
     with open("./cobaltcoastlands_.json", "wt", encoding="utf-8") as fout:
         fout.write(json.dumps(spawntable, ensure_ascii=False))
 
-    for tableId in spawntable["pokeball"].keys():
+    # for tableId in spawntable["pokeball"].keys():
 
-        table_text = requests.get(
-            f"https://www.serebii.net/pokearth/hisui/spawntable/{tableId}.txt"
-        )
-        data = table_text.text.replace("</tr> </tr>", "</tr>").replace(
-            "<br /><br />", ""
-        )
-        soup = bs(data, "html.parser")
+    #     table_text = requests.get(
+    #         f"https://www.serebii.net/pokearth/hisui/spawntable/{tableId}.txt"
+    #     )
+    #     data = table_text.text.replace("</tr> </tr>", "</tr>").replace(
+    #         "<br /><br />", ""
+    #     )
+    #     soup = bs(data, "html.parser")
 
-        clean_table = {}
-        for i, table in enumerate(soup.select("table")):
-            if i == 0:
-                continue
+    #     clean_table = {}
+    #     for i, table in enumerate(soup.select("table")):
+    #         if i == 0:
+    #             continue
 
-            info = []
-            key = None
-            for j, row in enumerate(table.select("tr")):
-                if j == 0:
-                    key = row.find("td").text
-                    clean_table[key] = []
-                elif j == 2:
-                    info.append(
-                        [
-                            (
-                                td.text,
-                                (True if td.select('img[alt="Alpha"]') else False),
-                            )
-                            for td in row.select("td")
-                        ]
-                    )
-                elif j == 4:
-                    info.append(
-                        [float(td.text.replace("%", "")) for td in row.select("td")]
-                    )
-                elif j == 6:
-                    info.append([td.text for td in row.select("td")])
+    #         info = []
+    #         key = None
+    #         for j, row in enumerate(table.select("tr")):
+    #             if j == 0:
+    #                 key = row.find("td").text
+    #                 clean_table[key] = []
+    #             elif j == 2:
+    #                 info.append(
+    #                     [
+    #                         (
+    #                             td.text,
+    #                             (True if td.select('img[alt="Alpha"]') else False),
+    #                         )
+    #                         for td in row.select("td")
+    #                     ]
+    #                 )
+    #             elif j == 4:
+    #                 info.append(
+    #                     [float(td.text.replace("%", "")) for td in row.select("td")]
+    #                 )
+    #             elif j == 6:
+    #                 info.append([td.text for td in row.select("td")])
 
-            for items in zip(*info):
-                clean_table[key].append(
-                    {
-                        "name": items[0][0],
-                        "alpha": items[0][1],
-                        "%": items[1],
-                        "level": items[2],
-                    }
-                )
+    #         for items in zip(*info):
+    #             clean_table[key].append(
+    #                 {
+    #                     "name": items[0][0],
+    #                     "alpha": items[0][1],
+    #                     "%": items[1],
+    #                     "level": items[2],
+    #                 }
+    #             )
 
-        # print(clean_table)
-        with open(f"./spawntable/{tableId}.json", "wt", encoding="utf-8") as fout:
-            fout.write(json.dumps(clean_table, ensure_ascii=False))
+    #     # print(clean_table)
+    #     with open(f"./spawntable/{tableId}.json", "wt", encoding="utf-8") as fout:
+    #         fout.write(json.dumps(clean_table, ensure_ascii=False))
