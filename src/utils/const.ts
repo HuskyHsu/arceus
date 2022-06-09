386b84c2f895f7440e8de24a944dc91c1407d22e
HuskyHsu <schi.hsu@gmail.com>
2022-06-09 22:08:18 +0800

    feat: 🎸 use jsDelivr cdn to load image and data

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = import.meta.env.BASE_URL;
+export const BASE_URL = import.meta.env.DEV
+  ? import.meta.env.BASE_URL
+  : "https://cdn.jsdelivr.net/gh/HuskyHsu/arceus@gh-pages/";
